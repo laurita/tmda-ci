bc433024f9631481c1f462e0c7e63161ce665ac7
Laura Bledaite <laura.bledaite@gmail.com>
2012-05-04 13:39:21 +0200

    Add file for creating sql tables.

--- tables.sql
+++ tables.sql
@@ -1,4 +1,99 @@
-CREATE TABLE employee (name int, salary int, TS int, TE int);
+CREATE TABLE random_02 (name int, salary int, TS int, TE int);
 
 .separator ";"
-.import data-random-02 test
+.import data-random-02 random_02
+
+CREATE TABLE random_04 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-random-04 random_04
+
+CREATE TABLE random_06 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-random-06 random_06
+
+CREATE TABLE random_08 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-random-08 random_08
+
+CREATE TABLE random_010 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-random-010 random_010
+
+CREATE TABLE equal_02 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-equal-02 equal_02
+
+CREATE TABLE equal_04 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-equal-04 equal_04
+
+CREATE TABLE equal_06 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-equal-06 equal_06
+
+CREATE TABLE equal_08 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-equal-08 equal_08
+
+CREATE TABLE equal_010 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-equal-010 equal_010
+
+CREATE TABLE seq_02 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-seq-02 seq_02
+
+CREATE TABLE seq_04 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-seq-04 seq_04
+
+CREATE TABLE seq_06 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-seq-06 seq_06
+
+CREATE TABLE seq_08 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-seq-08 seq_08
+
+CREATE TABLE seq_010 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-seq-010 seq_010
+
+CREATE TABLE worst_02 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-worst-02 worst_02
+
+CREATE TABLE worst_04 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-worst-04 worst_04
+
+CREATE TABLE worst_06 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-worst-06 worst_06
+
+CREATE TABLE worst_08 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-worst-08 worst_08
+
+CREATE TABLE worst_010 (name int, salary int, TS int, TE int);
+
+.separator ";"
+.import data-worst-010 worst_010
