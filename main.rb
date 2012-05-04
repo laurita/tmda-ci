66e16380d31e25f7d990f0ff908058d02b8b89e1
Laura Bledaite <laura.bledaite@gmail.com>
2012-05-04 13:38:10 +0200

    Add runtime tests.

---
+++ main.rb
@@ -0,0 +1,32 @@
+# running tests
+$LOAD_PATH.unshift File.dirname(__FILE__)
+
+require 'sqlite3'
+require 'tsd'
+
+def runtimetest()
+  db = SQLite3::Database.open('data/tsd.db')
+  g = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
+  f = ["sum",1]
+  theta=[[0,0]]
+  data_size = [2, 4, 6, 8, 10]
+  data_type = ["equal", "seq", "random", "worst"]
+  times = {}
+  data_type.each do |type| 
+    times[type] = {}
+    puts "Runtime test for #{type} data:"
+    data_size.each do |i| 
+      r = db.execute("select * from #{type}_0#{i};")
+      size = 100000 * i
+      puts "Testing for size #{size}..."
+      start_time = Time.now
+      tmda_ci(g,r,f,theta).inspect
+      duration = Time.now - start_time
+      times[type][size] = duration
+      puts "TMDA-CI took #{duration} time with data of #{size}"
+    end
+  end
+  times
+end
+
+puts runtimetest()
