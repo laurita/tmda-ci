964aea86a9da08a58814cce1108107b0279e39c0
Laura Bledaite <laura.bledaite@gmail.com>
2012-06-04 21:50:50 +0200

    Modify tmda_fi to be of similar input with tmda_ci.

--- tc_tmda_fi.rb
+++ tc_tmda_fi.rb
@@ -9,13 +9,8 @@ class TestTsd < Test::Unit::TestCase
   def test_lookup_fi
     tuple1 = [1, 2400, 1, 15]
     tuple2 = [1, 500, 1, 5]
-    gt = [
-          {:attr=>1, :start=>1, :end=>12, :function=>0}, 
-          {:attr=>1, :start=>13, :end=>24, :function=>0}, 
-          {:attr=>2, :start=>1, :end=>12, :function=>0}, 
-          {:attr=>2, :start=>13, :end=>24, :function=>0}
-         ]
-    theta = [{:r_col_nr => 0, :gt_attr_name => :attr}]
+    gt = [[1, 1, 12, 0], [1, 13, 24, 0], [2, 1, 12, 0], [2, 13, 24, 0]]
+    theta = [0, 0]
     looked_up_rows1 = [0, 1]
     looked_up_rows2 = [0] 
     assert_equal(looked_up_rows1, lookup_fi(gt, tuple1, theta))
@@ -24,18 +19,15 @@ class TestTsd < Test::Unit::TestCase
   
   def test_init_gt_fi
     g = [[1, 1, 12], [1, 13, 24], [2, 1, 12], [2, 13, 24]]
-    gt = [{:attr=>1, :start=>1, :end=>12, :function=>0}, 
-          {:attr=>1, :start=>13, :end=>24, :function=>0}, 
-          {:attr=>2, :start=>1, :end=>12, :function=>0}, 
-          {:attr=>2, :start=>13, :end=>24, :function=>0}]
+    gt = [[1, 1, 12, 0], [1, 13, 24, 0], [2, 1, 12, 0], [2, 13, 24, 0]]
     assert_equal(gt, init_gt_fi(g))
   end
   
   def test_adjust
     tuple = [1, 2400, 1, 15]
-    gt_i = {:attr=>1, :start=>1, :end=>12, :function=>0}
+    gt_i = [1, 1, 12, 0]
     c = "m"
-    r_prime = {:attr=>1, :start=>1, :end=>12, :function=>1920}
+    r_prime = [1, 1, 12, 1920]
     assert_equal(r_prime, adjust(tuple, gt_i, c))
   end
   
@@ -46,14 +38,9 @@ class TestTsd < Test::Unit::TestCase
          [1, 600, 13, 24], [1, 400, 1, 10], [2, 1200, 4, 10], [2, 900, 13, 18]
         ]
     f = "sum"
-    theta = [{:r_col_nr => 0, :gt_attr_name => :attr}]
+    theta = [0, 0]
     c = "m"
-    result = [
-              {:attr=>1, :start=>1, :end=>12, :function=>3520},
-              {:attr=>1, :start=>13, :end=>24, :function=>1980},
-              {:attr=>2, :start=>1, :end=>12, :function=>1200},
-              {:attr=>2, :start=>13, :end=>24, :function=>900}
-             ]
+    result = [[1, 1, 12, 3520], [1, 13, 24, 1980], [2, 1, 12, 1200], [2, 13, 24, 900]]
     assert_equal(result, tmda_fi(g,r,f,theta,c))
   end
 end
--- tmda_fi.rb
+++ tmda_fi.rb
@@ -5,16 +5,19 @@ NegInfinity = -1.0/0
 def init_gt_fi(g)
   gt = []
   g.each do |x|
-    hash = {:attr => x[0], :start => x[1], :end => x[2], :function => 0}
-    gt << hash
+    # list = [join attribute, start, end, function value]
+    list = [x[0], x[1], x[2], 0]
+    gt.push(list)
   end
   gt
 end
 
 def lookup_fi(gt, tuple, theta)
   lookup = []
+  gt_start = gt[0].length - 3
+  gt_end = gt[0].length - 2 
   gt.each_with_index do |row_gt, index| 
-    if (theta.all? {|cond| row_gt[cond[:gt_attr_name]] == tuple[cond[:r_col_nr]]} and (row_gt[:start] <= tuple.last) and (row_gt[:end] >= tuple[tuple.length - 2]))
+    if (theta.all? {|cond| row_gt[cond[0]] == tuple[cond[1]]} and (row_gt[gt_start] <= tuple.last) and (row_gt[gt_end] >= tuple[tuple.length - 2]))
       lookup.push(index)
     end
   end
@@ -22,15 +25,17 @@ def lookup_fi(gt, tuple, theta)
 end
 
 def adjust(tuple, gt_i, c)
+  gt_start = gt_i.length - 3
+  gt_end = gt_i.length - 2
   if (c == "m")
     #puts "tuple: #{tuple.inspect}"
-    start = [gt_i[:start], tuple[tuple.length - 2]].max
+    start = [gt_i[gt_start], tuple[tuple.length - 2]].max
     #puts "start: #{start}"
-    finish = [gt_i[:end], tuple.last].min
+    finish = [gt_i[gt_end], tuple.last].min
     #puts "finish: #{finish}"
-    gt_i[:function] += tuple[1] * (finish - start + 1) / (tuple.last - tuple[tuple.length - 2] + 1)
+    gt_i[gt_i.length - 1] += tuple[1] * (finish - start + 1) / (tuple.last - tuple[tuple.length - 2] + 1)
   elsif (c == "c")
-    gt_i[:function] += tuple[1]
+    gt_i[gt_i.length - 1] += tuple[1]
   end
   gt_i
 end
@@ -48,12 +53,26 @@ def tmda_fi(g,r,f,theta,c)
   gt
 end
 
+def make_group_table(g, r, number_of_groups)
+  number_of_intervals_in_group = (number_of_groups / g.length)
+  subintervals = []
+  interval_end = r.max_by {|x| x[x.length - 1]}.last
+  interval_start = r.min_by {|x| x[x.length - 2]}.last(2)[0]
+  interval_length = interval_end - interval_start + 1
+  subinterval_length = interval_length / number_of_intervals_in_group
+  range = Range.new(interval_start, interval_end)
+  range.step(subinterval_length) {|x| subintervals.push([x, x + subinterval_length - 1])}
+  puts subintervals.inspect
+end
+
+# select .. => [[1], [2]]; N = 2; SELECT MIN, MAX = 1, 24; [1,12], [13,24] 
 g = [[1, 1, 12], [1, 13, 24], [2, 1, 12], [2, 13, 24]]
 r = [
      [1, 2400, 1, 15], [1, 600, 19, 21], [1, 500, 1, 5], [1, 1000, 6, 15],
      [1, 600, 13, 24], [1, 400, 1, 10], [2, 1200, 4, 10], [2, 900, 13, 18]
     ]
 f = "sum"
-theta = [{:r_col_nr => 0, :gt_attr_name => :attr}]
-c = "c"
+
+theta = [0, 0]
+c = "m"
 puts tmda_fi(g,r,f,theta,c).inspect
