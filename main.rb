6ce9d50a004b331e08448bf28bdfe28408118e89
Laura Bledaite <laura.bledaite@gmail.com>
2012-06-04 13:57:34 +0200

    Modify tmda_fi and its tests.

--- main.rb
+++ main.rb
@@ -3,19 +3,21 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 
 require 'sqlite3'
 require 'tsd'
+require 'tmda_fi'
 
 def runtimetest()
   db = SQLite3::Database.open('data/tsd.db')
   g = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
   f = ["sum",1]
   theta=[[0,0]]
-  c="c"
+  c = "c"
   if c == "c"
     puts "CONSTANT ATTRIBUTES"
   elsif c == "m"
     puts "MALLABLE ATTRIBUTES"
   end
-  data_size = [2, 4, 6, 8, 10]
+  #data_size = [2, 4, 6, 8, 10]
+  data_size = [10]
   #data_type = ["equal", "seq", "random", "worst"]
   data_type = ["worst"]
   times = {}
@@ -30,10 +32,46 @@ def runtimetest()
       tmda_ci_new(g,r,f,theta,c).inspect
       duration = Time.now - start_time
       times[type][size] = duration
-      puts "TMDA-CI took #{duration} time with data of #{size}"
+      puts "TMDA-CI took #{duration} seconds with data of #{size}"
     end
   end
   times
 end
 
+=begin
+def runtimetest()
+  db = SQLite3::Database.open('data/tsd.db')
+  g = [[0, 1, 500], [0, 501, 1000], [1, 1, 500], [1, 501, 1000], 
+       [2, 1, 500], [2, 501, 1000], [3, 1, 500], [3, 501, 1000], 
+       [4, 1, 500], [4, 501, 1000], [5, 1, 500], [5, 501, 1000], 
+       [6, 1, 500], [6, 501, 1000], [7, 1, 500], [7, 501, 1000], 
+       [8, 1, 500], [8, 501, 1000], [9, 1, 500], [9, 501, 1000]]
+  f = "sum"
+  theta = [{:r_col_nr => 0, :gt_attr_name => :attr}]
+  c = "c"
+  if c == "m"
+    puts "CONSTANT ATTRIBUTES"
+  elsif c == "m"
+    puts "MALLABLE ATTRIBUTES"
+  end
+  data_size = [2, 4, 6, 8, 10]
+  data_type = ["equal", "seq", "random", "worst"]
+  times = {}
+  data_type.each do |type| 
+    times[type] = {}
+    puts "Runtime test for #{type} data:"
+    data_size.each do |i| 
+      r = db.execute("select * from #{type}_0#{i};")
+      size = 100000 * i
+      puts "Testing for size #{size}..."
+      start_time = Time.now
+      tmda_fi(g,r,f,theta,c).inspect
+      duration = Time.now - start_time
+      times[type][size] = duration
+      puts "TMDA-FI took #{duration} seconds with data of #{size}"
+    end
+  end
+  times
+end
+=end
 puts runtimetest()
--- tmda_fi.rb
+++ tmda_fi.rb
@@ -21,25 +21,39 @@ def lookup_fi(gt, tuple, theta)
   return lookup
 end
 
-def adjust(tuple, gt_i, theta)
-  #puts "tuple: #{tuple.inspect}"
-  start = [gt_i[:start], tuple[tuple.length - 2]].max
-  #puts "start: #{start}"
-  finish = [gt_i[:end], tuple.last].min
-  #puts "finish: #{finish}"
-  gt_i[:function] += tuple[1] * (finish - start + 1) / (tuple.last - tuple[tuple.length - 2] + 1)
+def adjust(tuple, gt_i, c)
+  if (c == "m")
+    #puts "tuple: #{tuple.inspect}"
+    start = [gt_i[:start], tuple[tuple.length - 2]].max
+    #puts "start: #{start}"
+    finish = [gt_i[:end], tuple.last].min
+    #puts "finish: #{finish}"
+    gt_i[:function] += tuple[1] * (finish - start + 1) / (tuple.last - tuple[tuple.length - 2] + 1)
+  elsif (c == "c")
+    gt_i[:function] += tuple[1]
+  end
   gt_i
 end
 
 def tmda_fi(g,r,f,theta,c)
   gt = init_gt_fi(g)
-  puts "gt: #{gt.inspect}"
+  #puts "gt: #{gt.inspect}"
   r.each do |tuple|
-    puts "tuple: #{tuple}"
+    #puts "tuple: #{tuple}"
     lookup_fi(gt,tuple,theta).each do |i|
       gt[i] = adjust(tuple,gt[i],c)
-      puts gt[i].inspect
+      #puts gt[i].inspect
     end  
   end
   gt
 end
+
+g = [[1, 1, 12], [1, 13, 24], [2, 1, 12], [2, 13, 24]]
+r = [
+     [1, 2400, 1, 15], [1, 600, 19, 21], [1, 500, 1, 5], [1, 1000, 6, 15],
+     [1, 600, 13, 24], [1, 400, 1, 10], [2, 1200, 4, 10], [2, 900, 13, 18]
+    ]
+f = "sum"
+theta = [{:r_col_nr => 0, :gt_attr_name => :attr}]
+c = "c"
+puts tmda_fi(g,r,f,theta,c).inspect
